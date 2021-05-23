11f53e6f7e95e2b73113b94f683cc26b8779e37f
Facundo Samartino <facundo.samartino@gmail.com>
2021-05-23 14:25:35 -0300

    GOT-51: Se agrega la tabla material

---
+++ DB/cooperativa_reciclaje.sql
@@ -0,0 +1,9 @@
+CREATE TABLE material (
+    nombre varchar(100)  NOT NULL,
+    imagen_src varchar(200)  NOT NULL,
+    descripcion varchar(2000)  NOT NULL,
+    CONSTRAINT material_pk PRIMARY KEY (nombre)
+);
+
+-- End of file.
+
