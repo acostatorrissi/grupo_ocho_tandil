af0c50aaf8db0bb2a94c0db97b0f5c681622a6e2
Facundo Samartino <facundo.samartino@gmail.com>
2021-05-24 11:56:36 -0300

    GOT-52: Se agrega la tabla de pedido al sql

--- DB/cooperativa_reciclaje.sql
+++ DB/cooperativa_reciclaje.sql
@@ -1,3 +1,15 @@
+CREATE TABLE Pedido (
+    id_pedido int  NOT NULL,
+    nombre varchar(200)  NOT NULL,
+    apellido varchar(200)  NOT NULL,
+    direccion varchar(200)  NOT NULL,
+    telefono varchar(50)  NOT NULL,
+    imagen_src varchar(500)  NOT NULL,
+    franja_horaria varchar(50)  NOT NULL,
+    volumen varchar(50)  NOT NULL,
+    CONSTRAINT Pedido_pk PRIMARY KEY (id_pedido)
+);
+
 CREATE TABLE material (
     nombre varchar(100)  NOT NULL,
     imagen_src varchar(200)  NOT NULL,
