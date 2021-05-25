137f4a2313b2f83f610e5218ff26312d91c28569
Leandro <lazarteleandro5@gmail.com>
2021-05-25 18:22:33 -0300

    feature/GOT-31: Se inserto item Tetrabrik en la tabla de materiales

--- DB/cooperativa_reciclaje.sql
+++ DB/cooperativa_reciclaje.sql
@@ -31,3 +31,8 @@ INSERT INTO `material` (`nombre`, `imagen_src`, `descripcion`) VALUES
 ('Envases plásticos', 
  'https://ichef.bbci.co.uk/news/640/cpsprodpb/C6DF/production/_97011905_gettyimages-103583960-1.jpg', 
  'Se aceptar plásticos derivados de botellas, (no se aceptarán envases de yogurt o queso blanco). Se deberá entregar limpias, secas y aplastadas.');
+
+INSERT INTO `material` (`nombre`, `imagen_src`, `descripcion`) VALUES
+('Tetrabrik', 
+ 'https://www.expoknews.com/wp-content/uploads/2019/05/Reciclado-de-tetrapack-en-buen-camino-pero-con-grandes-retos-1.jpg', 
+ 'Todo envase de este material será aceptado (bebidas y alimentos). Deberán ser entregados limpios, secos y aplastados.');
