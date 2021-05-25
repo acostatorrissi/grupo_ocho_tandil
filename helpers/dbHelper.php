d29d00ab22a5c92af7cfd1922e61582873823dcd
agustin-simon <agusstinbj@gmail.com>
2021-05-25 20:11:44 -0300

    GOT-61: Se agrega el campo mail

--- helpers/dbHelper.php
+++ helpers/dbHelper.php
@@ -12,5 +12,5 @@ class dbHelper {
         $db = new PDO("mysql:host={$this->host};"."dbname={$this->database};charset=utf8", 
                         $this->user, $this->password);
         return $db;
-    }    
+    }   
 }
