1332f0a6a8bfebe17c3e3ff6f0c864dd13581742
Facundo Samartino <facundo.samartino@gmail.com>
2021-06-27 23:02:18 -0300

    GOT-98: Se agrega el controller para agregar un cartonero

--- Controller.php
+++ Controller.php
@@ -79,6 +79,23 @@ class Controller{
          $this->view->FormularioLocation();
     }
 
+    function insertarCartonero(){
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $dni = $_POST['dni'];
+        $direccion = $_POST['direccion'];
+        $fechaNacimiento = $_POST['fechaNacimiento'];
+        $vehiculo = $_POST['vehiculo'];
+
+        if (!empty($nombre)&&!empty($apellido)&&!empty($direccion)
+            &&!empty($dni)&&!empty($fechaNacimiento)&&!empty($vehiculo)) {
+
+            $this->model->insertCartonero($nombre,$apellido,$dni,$direccion,$fechaNacimiento,$vehiculo);
+        }
+        //TODO redirigir al listado de cartoneros
+         $this->view->FormularioCartoneroLocation();
+    }
+
     function uploadImage($rutaTemp,$nombreImagen){
         $img = uniqid() . "." . pathinfo($nombreImagen, PATHINFO_EXTENSION);
         move_uploaded_file($rutaTemp,"img/".$img);
